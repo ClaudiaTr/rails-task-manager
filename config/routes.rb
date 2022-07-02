bece7f8096b21f4beda4c3dd8ecf544aa9b2d62a
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-07-02 15:24:07 +0100

    Exercise 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read all tasks
   get "tasks", to: "tasks#index"
-  # display one task
-  get "tasks/:id", to: "tasks#show"
   # create a task
   get "tasks/new", to: "tasks#new" # display form
   post "tasks", to: "tasks#create" # submit new task
+  # display one task
+  get "tasks/:id", to: "tasks#show", as: "task"
+  delete "tasks/:id", to: "tasks#destroy"
   # edit a task
-  get "tasks/:id/edit", to: "tasks#edit" # display form
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit" # display form
   patch "tasks/:id", to: "tasks#update" # submit update
   # delete a task
-  delete "tasks/:id", to: "tasks#destroy"
 end
