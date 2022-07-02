21b4f41fbf6df520b20c8ee188a69f63da051152
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-07-02 11:56:47 +0100

    Created all routes, actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read all tasks
+  get "tasks", to: "tasks#index"
+  # display one task
+  get "tasks/:id", to: "tasks#show"
+  # create a task
+  get "tasks/new", to: "tasks#new" # display form
+  post "tasks", to: "tasks#create" # submit new task
+  # edit a task
+  get "tasks/:id/edit", to: "tasks#edit" # display form
+  patch "tasks/:id", to: "tasks#update" # submit update
+  # delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
